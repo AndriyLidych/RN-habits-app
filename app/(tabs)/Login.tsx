1eac170980f5a700607c6e0a6e4902fa3cbe199b
AndriyLidych <lidichandre@gmail.com>
2025-09-18 11:16:28 +0300

    first commit

---
+++ app/(tabs)/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>12312312312312</Text>
+        </View>
+    );
+};
+
+export default LoginScreen;
